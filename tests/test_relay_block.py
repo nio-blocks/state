dadb72609d52eedd1bdcbedd93dee68892325e5a
mattdodge <matt@n.io>
2015-03-05 12:02:28 -0800

    Adds/cleans up relay block tests

--- tests/test_relay_block.py
+++ tests/test_relay_block.py
@@ -1,22 +1,19 @@
 from unittest.mock import patch
 from nio.util.support.block_test_case import NIOBlockTestCase
+from .test_state_base_block import StateSignal
 from nio.common.signal.base import Signal
 from ..relay_block import Relay
 
 
-class StateSignal(Signal):
-    def __init__(self, state):
-        super().__init__()
-        self.state = state
-
-
 class OtherSignal(Signal):
+
     def __init__(self, state):
         super().__init__()
         self.other = state
 
 
 class TestRelay(NIOBlockTestCase):
+
     def get_test_modules(self):
         return self.ServiceDefaultModules + ['persistence']
 
@@ -25,49 +22,44 @@ class TestRelay(NIOBlockTestCase):
 
     @patch.object(Relay, '_backup')
     def test_relay(self, mock_backup):
-        print("Testing Relay")
         blk = Relay()
-        config = {
-            "state_expr": "{{$state}}"
-        }
-        self.configure_block(blk, config)
+        self.configure_block(blk, {
+            'state_sig': '{{hasattr($, "state")}}',
+            'state_expr': '{{$state}}',
+            'initial_state': '{{False}}',
+            'group_by': 'null'
+        })
         blk.start()
 
-        signals_notified = 0
-
-        # state is initally False
-        print(blk._state)
-        self.assertFalse(bool(blk._state))
+        self.assertFalse(blk.get_state('null'))
 
         # set state to True
         blk.process_signals([StateSignal('1')])
-        self.assertEqual(blk._state, '1')
-        self.assertTrue(bool(blk._state))
-        self.assert_num_signals_notified(signals_notified, blk)
+        self.assertEqual(blk.get_state('null'), '1')
+        self.assertTrue(bool(blk.get_state('null')))
+        self.assert_num_signals_notified(0, blk)
 
         # send a true state + other signal
         blk.process_signals([StateSignal('2'), OtherSignal('3')])
-        signals_notified += 1
-        self.assert_num_signals_notified(signals_notified, blk)
-        self.assertEqual(blk._state, '2')
-        self.assertTrue(bool(blk._state))
+        self.assert_num_signals_notified(1, blk)
+        self.assertEqual(blk.get_state('null'), '2')
+        self.assertTrue(bool(blk.get_state('null')))
         self.assertEqual(self._signals[0].other, '3')
 
         # no signals pass through when State is false
         blk.process_signals([StateSignal(False), OtherSignal('4')])
-        self.assertFalse(bool(blk._state))
-        self.assert_num_signals_notified(signals_notified, blk)
+        self.assertFalse(bool(blk.get_state('null')))
+        self.assert_num_signals_notified(1, blk)
 
         # no signals still pass through
         blk.process_signals([OtherSignal(n) for n in range(100)])
-        self.assertEqual(blk._state, False)
-        self.assert_num_signals_notified(signals_notified, blk)
+        self.assertEqual(blk.get_state('null'), False)
+        self.assert_num_signals_notified(1, blk)
 
         # set state to 1 and get notification signal.
         blk.process_signals([StateSignal('1'), OtherSignal('5')])
-        signals_notified += 1
-        self.assert_num_signals_notified(signals_notified, blk)
-        self.assertEqual(blk._state, '1')
-        self.assertTrue(bool(blk._state))
+        self.assert_num_signals_notified(2, blk)
+        self.assertEqual(blk.get_state('null'), '1')
+        self.assertTrue(bool(blk.get_state('null')))
         self.assertEqual(self._signals[0].other, '5')
         blk.stop()
