5ae86eff93359d5dcb13e6a7c6de5c6523f9420a
Chris Brackert <cbrackert@gmail.com>
2015-09-01 10:41:42 -0700

    Add state_name property for specifying signal attribute names
    Close #16

--- tests/test_state_change_block.py
+++ tests/test_state_change_block.py
@@ -27,12 +27,10 @@ class TestStateChange(NIOBlockTestCase):
             'group_by': '{{$group}}'
         })
         blk.start()
-
         # init state to 1
         blk.process_signals([StateSignal('1', 'A')])
         self.assertEqual(blk.get_state('A'), '1')
         self.assert_num_signals_notified(1, blk)
-
         # set state to 2 and get notification signal.
         blk.process_signals([StateSignal('2', 'A')])
         self.assertEqual(blk.get_state('A'), '2')
@@ -40,19 +38,16 @@ class TestStateChange(NIOBlockTestCase):
         self.assertEqual(self._signals[0].prev_state, '1')
         self.assertEqual(self._signals[0].state, '2')
         self.assertEqual(self._signals[0].group, 'A')
-
         # no notification when state does not change.
         blk.process_signals([StateSignal('2', 'A')])
         self.assertEqual(blk.get_state('A'), '2')
         self.assert_num_signals_notified(2, blk)
-
         # set state to 1 and get notification signal.
         blk.process_signals([StateSignal('1', 'A')])
         self.assertEqual(blk.get_state('A'), '1')
         self.assert_num_signals_notified(3, blk)
         self.assertEqual(self._signals[0].prev_state, '2')
         self.assertEqual(self._signals[0].state, '1')
-
         blk.stop()
 
     def test_no_exclude(self, mock_backup):
@@ -65,10 +60,8 @@ class TestStateChange(NIOBlockTestCase):
             'exclude': False
         })
         blk.start()
-
         # init state to 1
         blk.process_signals([StateSignal('1', 'A')])
-
         # set state to 2 and get notification signal.
         blk.process_signals([StateSignal('2', 'A')])
         self.assertEqual(blk.get_state('A'), '2')
@@ -77,8 +70,19 @@ class TestStateChange(NIOBlockTestCase):
         self.assertEqual(self._signals[0].state, '2')
         # Make sure that attributes on the original signal also came through
         self.assertEqual(self._signals[0].group, 'A')
-
         # no notification when state does not change.
         blk.process_signals([StateSignal('2', 'A')])
         self.assertEqual(blk.get_state('A'), '2')
         self.assert_num_signals_notified(2, blk)
+
+    def test_state_name_property(self, mock_backup):
+        """ Tests that state attribute can be customized"""
+        blk = StateChange()
+        self.configure_block(blk, {
+            'state_name': 'attr'
+        })
+        blk.start()
+        # init state to 1
+        blk.process_signals([StateSignal(1, 'A')])
+        self.assertDictEqual(self._signals[0].to_dict(),
+                             {'prev_attr': None, 'attr': 1, 'group': 'null'})
