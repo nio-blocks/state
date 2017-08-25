5fd9383bcc888ed81239e8cb5b1c5dd2597f7877
neutralioJenkins <jenkins@n.io>
2017-08-25 13:58:16 -0600

    Bump version to v0.1.0

--- append_state_block.py
+++ append_state_block.py
@@ -14,7 +14,7 @@ class AppendState(StateBase):
     """
 
     state_name = StringProperty(default='state', title="State Name")
-    version = VersionProperty('0.1.0')
+    version = VersionProperty("0.1.0")
 
     def _process_group(self, signals, group, input_id, signals_to_notify):
         if input_id == 'setter':
